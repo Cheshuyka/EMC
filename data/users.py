edbf920e5924224d65a8478029a1aa367aa4a83a
Cheshuyka <cheshuyka.ref@gmail.com>
2023-04-12 11:05:30 +0300

    made login, register, main page

---
+++ data/users.py
@@ -0,0 +1,24 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy_serializer import SerializerMixin
+
+
+class User(SqlAlchemyBase, UserMixin, SerializerMixin):
+    __tablename__ = 'users'
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    surname = sqlalchemy.Column(sqlalchemy.String)
+    name = sqlalchemy.Column(sqlalchemy.String)
+    school = sqlalchemy.Column(sqlalchemy.Integer)
+    classClub = sqlalchemy.Column(sqlalchemy.String)
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    verified = sqlalchemy.Column(sqlalchemy.Boolean)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
---
+++ main.py
@@ -0,0 +1,101 @@
+from EMC.data import db_session
+from flask import Flask, render_template, redirect
+from flask_login import LoginManager, login_user, login_required, logout_user
+from EMC.data.users import User
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, StringField, BooleanField, SubmitField, EmailField, PasswordField
+from wtforms.validators import DataRequired
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you_know_its_a_secret_key_really_secret'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class RegisterForm(FlaskForm):
+    name = StringField('Имя', validators=[DataRequired()])
+    surname = StringField('Фамилия', validators=[DataRequired()])
+    school = IntegerField('ID школы', validators=[DataRequired()])
+    classNum = IntegerField('Номер класса', validators=[DataRequired()])
+    classLet = StringField('Буква класса', validators=[DataRequired()])
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    submit = SubmitField('Войти')
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_session.global_init('db/EMC.db')
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/')
+def index():
+    return render_template('base.html', title='EMC')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_session.global_init('db/EMC.db')
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            surname=form.surname.data,
+            name=form.name.data,
+            school=form.school.data,
+            classClub=str(form.classNum.data) + form.classLet.data,
+            verified=False,
+            email=form.email.data)
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        login_user(user)
+        return redirect('/')
+    return render_template('register.html', title='Авторизация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_session.global_init('db/EMC.db')
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+if __name__ == '__main__':
+    app.run()
